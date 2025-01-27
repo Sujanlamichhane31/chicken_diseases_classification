62bb379ee4a91f52b05ff4111b3f40ef3de95bca
unknown <sujanlamichhane742@gmail.com>
2025-01-26 20:58:48 -0500

    folder structure added

--- template.py
+++ template.py
@@ -13,6 +13,7 @@ list_of_files = [
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
@@ -21,7 +22,9 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trails.ipynb",
-    "templates/index.html"
+    "templates/index.html",
+    "test.py"
+
 
 
 ]
